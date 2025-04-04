f8bf5a0d3fdd37ff6cff6f7d01fc02d0f85a0f30
Amanda Machado <amandabuenomachado46@gmail.com>
2025-04-04 19:30:45 -0300

    Update programa.py

--- codigo.py/programa.py
+++ codigo.py/programa.py
@@ -1,8 +1,8 @@
 idade = int(input("Digite sua idade: "))
-convite = input("Você tem convite? (sim/nao): ").strip().lower()
+convite = input("Você tem convite? (sim/nao): ")
 
 if idade >= 18:
-    if convite == 's':
+    if convite == 'sim':
         print("Entrada permitida! Aproveite o evento")
     else:
         print("Desculpe, você precisa de um convite para entrar")
